5b33c45dece56dfbba8ec34c8e1ec524961855ba
Alison <104040953+AlisonFDLHC@users.noreply.github.com>
2022-08-10 11:23:28 -0300

    v3

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,37 +1,54 @@
 const botoes = document.querySelectorAll('.botao')
 const lampada = document.getElementById('bulbo')
 var intensidade = 1 
-var estadolampada = 0 
-var coratual = '255,255,255' 
-var trocandocorflash = false 
-var trocandocorstrobe = false 
+var lampadaLigada = false
+var corAtual = '255,255,255' 
+var trocandoCorFade = false 
+var trocandoCorStrobe = false 
 
-function trocacorflash(){
-    var cores = ["255, 0, 0", "0, 128, 0", "0, 0, 255", "163, 48, 48", "63, 150, 63","57, 57, 180","196, 62, 0","0, 139, 53","65, 0, 216"]; 
+var cores = 
+            [
+                "255, 0, 0",
+                "0, 128, 0",
+                "0, 0, 255",
+                "235, 31, 133",
+                "22, 172, 209",
+                "146, 45, 197",
+                "255, 0, 64",
+                "0, 255, 242",
+                "38, 0, 255"
+            ];
+
+function trocaCorFade(){
     cor = 0;
-    div = 0;
-    flash = setInterval(function(){
-                cor++; div++;
-                if (cor >= cores.length){ 
-                    cor=0;}
-                coratual = cores[cor] 
-                lampada.style.backgroundColor = `rgba(${cores[cor]},${intensidade}` 
-                lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})` 
-            }, 800) 
+    intensidade = 1;
+    fade = setInterval(() => 
+                {
+                    corAtual = cores[cor]
+                    if(intensidade > 0) {
+                        intensidade -= 0.01
+                    } else {
+                        cor++;
+                        corAtual = cores[cor]
+                        if (cor >= cores.length) { cor = 0;}
+                        intensidade = 1
+                    }
 
+                    lampada.style.backgroundColor = `rgba(${corAtual},${intensidade}` 
+                    lampada.style.boxShadow = `1px 1px 50px rgba(${corAtual},${intensidade})`
+                }
+                ,40)
 }
 
-function trocacorstrobe(){
-    var cores = ["255, 0, 0", "0, 128, 0", "0, 0, 255", "163, 48, 48", "63, 150, 63","57, 57, 180","196, 62, 0","0, 139, 53","65, 0, 216"];
+function trocaCorStrobe(){
     cor = 0;
-    div = 0;
     strobe = setInterval(function(){
-                cor++; div++;
+                cor++;
                 if (cor >= cores.length){ 
                     cor=0;}
-                coratual = cores[cor] 
+                corAtual = cores[cor] 
                 lampada.style.backgroundColor = `rgba(${cores[cor]},${intensidade}` 
-                lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})` 
+                lampada.style.boxShadow = `1px 1px 50px rgba(${corAtual},${intensidade})` 
             }, 100) 
 
 }
@@ -43,200 +60,104 @@ botoes.forEach((botao) => {
 function botaofuncao(funcao){
     switch (funcao){
         case "on": 
-            if (estadolampada == 0){  
+            if (lampadaLigada == false){
+                lampadaLigada = true 
                 intensidade = 1 
-                estadolampada = 1 
-                trocandocorflash = false 
-                trocandocorstrobe = false 
-                clearInterval(flash) 
-                clearInterval(strobe) 
-                lampada.style.backgroundColor = `rgba(${coratual},${intensidade})` 
-                lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})` 
             }
         break;
 
         case "off": 
-            estadolampada = 0 
-            trocandocorflash = false 
-            trocandocorstrobe = false 
-            clearInterval(flash) 
-            clearInterval(strobe) 
-            lampada.style.backgroundColor = `rgba(${coratual},0)` 
+            lampadaLigada = false 
+            lampada.style.backgroundColor = `rgba(${corAtual},0)` 
             lampada.style.boxShadow = `` 
         break;
 
         case "luzmenos": 
-            if (estadolampada == 1){ 
-                if (intensidade >= 0){ 
+            if (lampadaLigada == true){ 
+                if (intensidade >= 0.2){ 
                     intensidade -= 0.1 
                 }
-                lampada.style.backgroundColor = `rgba(${coratual},${intensidade})` 
-                lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})` 
             }
         break;
         
         case "luzmais": 
-            if (estadolampada == 1){ 
+            if (lampadaLigada == true){ 
                 if (intensidade <= 1){ 
                     intensidade += 0.1 
                 }
-                lampada.style.backgroundColor = `rgba(${coratual},${intensidade})` 
-                lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})` 
             }
-        break;
-
-        case "red1":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "255,0,0" 
-            trocandocorflash = false 
-            trocandocorstrobe = false 
-            clearInterval(flash) 
-            clearInterval(strobe) 
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})` 
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})` 
-        break;
-
-        case "green1":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "0,128,0"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
-        break;
-
-        case "blue1":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "0,0,255"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
         break;
 
         case "white":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "255,255,255"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
+            corAtual = '255,255,255'
         break;
 
-        case "red2":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "163, 48, 48"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
-        break;
-
-        case "green2":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "63, 150, 63"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
-        break;
-
-        case "blue2":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "57, 57, 180"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
-        break;
-
-        case "flash":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            } 
-            if (estadolampada == 1 && trocandocorflash == false){ 
+        case "fade":
+            if (lampadaLigada == true && trocandoCorFade == false){ 
                 clearInterval(strobe) 
-                trocacorflash() 
-                trocandocorflash = true 
-                trocandocorstrobe = false 
+                trocaCorFade() 
+                trocandoCorFade = true 
+                trocandoCorStrobe = false 
             }
         break;
         
         case "strobe":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            } 
-            if (estadolampada == 1 && trocandocorstrobe == false){ 
-                clearInterval(flash) 
-                trocacorstrobe() 
-                trocandocorstrobe = true 
-                trocandocorflash = false 
-        }
+            if (lampadaLigada == true && trocandoCorStrobe == false){ 
+                clearInterval(fade) 
+                trocaCorStrobe() 
+                trocandoCorStrobe = true 
+                trocandoCorFade = false 
+            }
+        break;
+
+        case "red1":
+            corAtual = cores[0]
+        break;
+
+        case "green1":
+            corAtual = cores[1]
+        break;
+
+        case "blue1":
+            corAtual = cores[2]
+        break;
+
+        case "red2":
+            corAtual = cores[3]
+        break;
+
+        case "green2":
+            corAtual = cores[4]
+        break;
+
+        case "blue2":
+            corAtual = cores[5]
         break;
 
         case "red3":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "196, 62, 0"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
+            corAtual = cores[6]
         break;
 
         case "green3":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "0, 139, 53"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
+            corAtual = cores[7]
         break;
         
         case "blue3":
-            if (estadolampada == 0) {
-                estadolampada = 1
-            }
-            coratual = "65, 0, 216"
-            trocandocorflash = false
-            trocandocorstrobe = false
-            clearInterval(flash)
-            clearInterval(strobe)
-            lampada.style.backgroundColor = `rgba(${coratual},${intensidade})`
-            lampada.style.boxShadow = `1px 1px 50px rgba(${coratual},${intensidade})`
+            corAtual = cores[8]
         break;
-    }
+
+        }
+
+        if (lampadaLigada == true) {
+            lampada.style.backgroundColor = `rgba(${corAtual},${intensidade})`
+            lampada.style.boxShadow = `1px 1px 50px rgba(${corAtual},${intensidade})`
+        }
+        
+
+        if (funcao !== 'strobe' && funcao !== 'fade' && funcao !== 'luzmais' && funcao !== 'luzmenos' && funcao !== 'on') {
+            trocandoCorFade = false
+            trocandoCorStrobe = false
+            clearInterval(fade)
+            clearInterval(strobe)
+        }
 }
